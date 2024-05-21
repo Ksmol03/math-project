d1003d6da528f2b6b109fce1d64845862054838c
Kacper <kacper.smolinski@hotmail.com>
2024-05-21 19:40:30 +0200

    add: Function that creates equation equals to 0

--- index2.js
+++ index2.js
@@ -2,6 +2,7 @@ const randInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 const X = randInt(-10, 11);
 const Y = randInt(-10, 11);
+const useFractions = true;
 
 //Create varibles placement sequence e.g.: [X, Y, Number, Number, X]
 const createSequence = () => {
@@ -44,6 +45,56 @@ const createSequence = () => {
 }
 
 //Assign values to variables positions
-const assignValues = (sequence) => {
-    
+const assignValues = (sequence) => (
+    sequence.map(position => {
+        if (useFractions) {
+            return ({
+                numerator: randInt(-10, 11),
+                variable: position,
+                denumerator: randInt(0, 3) != 0 ? randInt(2, 7) : 'None'
+            })
+        } else {
+            return ({
+                numerator: randInt(-10, 11),
+                variable: position,
+                denumerator: 'None'
+            })
+        }
+    })
+)
+
+//Calculate sequence with values
+const calculateSequence = (sequence) => {
+    return sequence.reduce((sum, mathWord) => {
+        let variableMultiplier;
+        switch (mathWord.variable) {
+            case 'X':
+                variableMultiplier = X;
+                break;
+            case 'Y':
+                variableMultiplier = Y;
+                break;
+            case 'Number':
+                variableMultiplier = 1;
+            break;
+        }
+        let mathWordDenumerator = mathWord.denumerator == 'None' ? 1 : mathWord.denumerator;
+        return sum + mathWord.numerator * variableMultiplier / mathWordDenumerator;
+    }, 0);
 }
+
+
+//Find equation that equals 0
+let sum;
+let mathEq;
+let attempts = 0;
+do {
+    mathEq = assignValues(createSequence());
+    sum = calculateSequence(mathEq);
+    attempts++;
+} while (sum != 0);
+
+console.log(mathEq);
+console.log('X: ', X);
+console.log('Y: ', Y);
+console.log(`Found in ${attempts} attempts.`);
